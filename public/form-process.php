71e262c0228dbef8c72b8b078d55db9290fdf397
“Jakub <jakub@chutnak.sk>
2025-06-27 09:33:06 +0200

    prepisanie na test

--- public/form-process.php
+++ public/form-process.php
@@ -1,73 +1,27 @@
 <?php
+$errorMSG = "";
 
-	$errorMSG = "";
+if (empty($_POST["email"])) {
+	$errorMSG .= "Email is required. ";
+} else {
+	$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$errorMSG .= "Invalid email format. ";
+	}
+}
 
-	// FNAME
-	if (empty($_POST["fname"])) {
-		$errorMSG = "First Name is required. ";
+$to = "chutnak.jakub@gmail.com";
+$subject = "Test cez formulár";
+$message = "Test z formulára na notari.chutnak.sk";
+$headers = "From: noreply@notari.chutnak.sk\r\n";
+$headers .= "Reply-To: " . $email;
+
+if ($errorMSG == "") {
+	if (mail($to, $subject, $message, $headers)) {
+		echo "✅ Odoslané";
 	} else {
-		$fname = $_POST["fname"];
+		echo "❌ Zlyhalo odoslanie";
 	}
-
-	// LNAME
-	if (empty($_POST["lname"])) {
-		$errorMSG = "Last Name is required. ";
-	} else {
-		$lname = $_POST["lname"];
-	}
-
-	// EMAIL
-	if (empty($_POST["email"])) {
-		$errorMSG .= "Email is required. ";
-	} else {
-		$email = $_POST["email"];
-	}
-
-	// PHONE
-	if (empty($_POST["phone"])) {
-		$errorMSG .= "Phone is required. ";
-	} else {
-		$phone = $_POST["phone"];
-	}
-
-	// MESSAGE
-	if (empty($_POST["message"])) {
-		$errorMSG .= "Message is required. ";
-	} else {
-		$message = $_POST["message"];
-	}
-
-	$subject = $_POST['subject'].' - Contact from site';
-
-	$EmailTo = "chutnak.jakub@gmail.com"; // Replace with your email.
-
-	// prepare email body text
-	$Body = "";
-	$Body .= "Name: ";
-	$Body .= $fname." ".$lname;
-	$Body .= "\n";
-	$Body .= "Email: ";
-	$Body .= $email;
-	$Body .= "\n";
-	$Body .= "Phone: ";
-	$Body .= $phone;
-	$Body .= "\n";
-	$Body .= "Message: ";
-	$Body .= $message;
-	$Body .= "\n";
-
-	// send email
-	$success = @mail($EmailTo, $subject, $Body, "From:".$email);
-
-	// redirect to success page
-	if ($success && $errorMSG == ""){
-	   echo "success";
-	}else{
-		if($errorMSG == ""){
-			echo "Something went wrong :(";
-		} else {
-			echo $errorMSG;
-		}
-	}
-
-?>
+} else {
+	echo $errorMSG;
+}
